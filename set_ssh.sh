a59c84fdbba7d981df63f41447855cf7f96b08f6
SKY <sky012877@gmail.com>
2023-05-31 16:32:51 -0700

    set_ssh.sh - Added :

---
+++ set_ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description :                                                            #
+############################################################################
+
+echo Creating symlink from .ssh to config/ssh
+
+ln -sfv $PWD/ssh $HOME/.ssh
