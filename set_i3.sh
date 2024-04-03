1e3f1b96f14834d3ef124c77446928090a09de71
SKY <sky012877@gmail.com>
2024-04-03 15:41:45 -0700

    set_i3.sh - Modified :

--- set_i3.sh
+++ set_i3.sh
@@ -6,46 +6,35 @@
 # Description :                                                            #
 ############################################################################
 
-echo Editing global i3 config ...
-sudo sed -i "s/exec i3-config-wizard//g" /etc/i3/config
+source "$EXE"
+start "$0"
 
-echo Creating symbolic link for user i3 config
 path=$HOME/.i3
-mkdir -vp $path
 
-[ -f $path/config ] && mv $path/config $path/config.old
+h1 Manjaro i3 and eco system setup
 
-ln -sfv $PWD/i3.config $path/config
-readlink $path/config
+h2 Editing global i3 config for removing config wizzard
+exe "sudo sed -i 's/exec i3-config-wizard//g' /etc/i3/config"
 
+h2 Creating .i3 directory for configs if absent
+exe "mkdir -vp $path; la -la $path"
 
-#cp  -vf i3.config $path/config
-ls -la $path
+h2 Saving existiong config file to .old version
+exe "[ -f $path/config ] && mv $path/config $path/config.old" -n
 
-echo Creating symbolic link for i3 .profile config ...
+h2 Creating symbolik link for i3.config
+exe "ln -sfv $PWD/i3.config $path/config && readlink $path/config; ls -la $path"
 
-ln -sfv $PWD/i3.profile $HOME/.profile
-#ls -la $HOME
-readlink $HOME/.profile
+h2 Creating symbolic link for i3 .profile config
+exe "ln -sfv $PWD/i3.profile $HOME/.profile && readlink $HOME/.profile"
 
+h2 Creating symbolic link for i3 mimeapps.list profile config
+exe "ln -sfv $PWD/mimeapps.list $HOME/.config/mimeapps.list && readlink $HOME/.config/mimeapps.list"
 
-#cp  -vf i3.config $path/config
-ls -la $path
+h2 Creating symbolic link for .pylintrc
+exe "ln -sfv $PWD/pylintrc $HOME/.pylintrc && readlink $HOME/.pylintrc"
 
-echo Creating symbolic link for i3 mimeapps.list  profile config ...
+h2 Creating symbolic link for .ideavimrc config ...
+exe "ln -sfv $PWD/ideavimrc $HOME/.ideavimrc readlink $HOME/.config/.ideavimrc"
 
-ln -sfv $PWD/mimeapps.list $HOME/.config/mimeapps.list
-#ls -la $HOME
-readlink $HOME/.config/mimeapps.list
-
-echo Creating symbolic link for .ideavimrc config ...
-
-ln -sfv $PWD/ideavimrc $HOME/.ideavimrc
-#ls -la $HOME
-readlink $HOME/.config/mimeapps.list
-
-
-
-echo Done!
-
-
+end "$0"
