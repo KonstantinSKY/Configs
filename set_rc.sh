1f295eb31793f637783e96f0b9bd396f215ab1ed
SKY <sky012877@gmail.com>
2024-04-04 12:23:29 -0700

    set_rc.sh - Modified :

--- set_rc.sh
+++ set_rc.sh
@@ -5,32 +5,28 @@
 # Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
 # Description : Add env vars and my rc part to bashrc and to others ..rs fi#
 ############################################################################
-chmod_list=""
 
 echo Adding additional environment variables an rc aliases
 
 link=". $PWD/rc"
 rc_files=".bashrc .zhsrc .zshrc"
 
-echo Copying $rc_files to Config directory..
+echo Copying "$rc_files" to Config directory..
 
+echo Adding link string "$link" to .rc files ...
+for file in $rc_files; do
+	file=$HOME/$file
+	echo Checking "$file" ...
+	if [[ ! -f $file ]]; then
+		echo File "$file" not found
+		echo Creating Empty "$file"
+		touch "$file"
+	fi
 
-echo Adding link string $link to .rc files ...
-for file in $rc_files
-    do
-		file=$HOME/$file
-		echo Checking $file ...
-		if [[ ! -f $file ]]; then
-			echo File $file not found
-			echo Creating Empty  $file
-			touch $file
-		fi
+	echo Trying to add rc link to "$file" ...
+	grep -q "$link" "$file" && echo "The $file already has: $link" || echo "$link" >>"$file"
+	echo Checking inside the file...
+	tail -n 5 "$file"
+	echo "==============="
 
-		echo Trying to add rc link to $file ...
-        grep -q "$link"	$file && echo "The $file already has: $link" || echo $link >> $file
-		echo Checking inside the file...
-		tail -n 5 $file
-		echo "==============="
-
-	done
-
+done
