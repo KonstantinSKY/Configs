dafafc5daaafc97c28666fdd57979c9732cc4359
sky <sky012877@gmail.com>
2026-03-03 14:36:44 -0800

    Refactor build system to use centralized global GNU Make configuration and simplify individual Makefiles.

---
+++ make/all.mk
@@ -0,0 +1,8 @@
+# Global GNU Make extensions
+# These files are found because of MAKEFLAGS="-I /home/sky/Configs/make"
+
+include colors.mk
+include help.mk
+include git.mk
+
+# Add more global modules here in the future
