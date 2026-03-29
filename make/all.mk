6ba16889f785dce8ae2781fa9c9f1b64973c80e1
sky <sky012877@gmail.com>
2026-03-28 20:11:53 -0700

    Use absolute paths in global make includes

--- make/all.mk
+++ make/all.mk
@@ -1,12 +1,14 @@
 # Global GNU Make extensions
 # These files are found because of MAKEFLAGS="-I /home/sky/Work/Configs/make"
 
-include colors.mk
-include fs.mk
-include backup.mk
-include stow.mk
-include help.mk
-include git.mk
-include update.mk
+MAKE_MODULE_DIR := $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
+
+include $(MAKE_MODULE_DIR)/colors.mk
+include $(MAKE_MODULE_DIR)/fs.mk
+include $(MAKE_MODULE_DIR)/backup.mk
+include $(MAKE_MODULE_DIR)/stow.mk
+include $(MAKE_MODULE_DIR)/help.mk
+include $(MAKE_MODULE_DIR)/git.mk
+include $(MAKE_MODULE_DIR)/update.mk
 
 # Add more global modules here in the future
